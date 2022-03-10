0ceb0a67a2a40c347dca9636a6ec1cf878c86f7a
zhonghuaifang <56331610+zhonghuaifang@users.noreply.github.com>
2022-03-10 12:45:25 +0800

    Update sendNotify.py

--- function/wps/sendNotify.py
+++ function/wps/sendNotify.py
@@ -61,6 +61,18 @@ class sendNotify:
     PUSH_PLUS_TOKEN = ''
     # PUSH_PLUS_USER： 一对多推送的“群组编码”（一对多推送下面->您的群组(如无则新建)->群组编码，如果您是创建群组人。也需点击“查看二维码”扫描绑定，否则不能接受群组消息推送）
     PUSH_PLUS_USER = ''
+    
+    # =======================================go-cqhttp设置区域=======================================
+    # go-cqhttp
+    # 推送到个人QQ: http://127.0.0.1/send_private_msg
+    # 群: http://127.0.0.1/send_group_msg
+    GOBOT_URL = ''
+    # go-cqhttp 的推送群或用户
+    # GOBOT_URL 设置 /send_private_msg 时填入 user_id=个人QQ
+    # /send_group_msg   时填入 group_id=QQ群
+    GOBOT_QQ = ''
+    # go-cqhttp 的 access_token
+    GOBOT_TOKEN = ''
 
     # Server酱
     if os.environ['PUSH_KEY'] != "":
